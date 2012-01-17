704af469889a7b8f0dfc5e50bb2d306448265c94
Nicolas CHARLERY <nicolas.charlery@gmail.com>
2012-01-17 00:46:39 -0800

    Retrieving visitor's address from Google

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.1.3'
+gem "geokit-rails31", "~> 0.1.3"
 
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,15 @@
 class HomeController < ApplicationController
-
   def index
   end
 
   def doThingWithGeoLocationVisitor
-   @message = params[:msg]
+   @lon = params[:lon]
+   @lat = params[:lat]
+
+   @info= Geokit::Geocoders::GoogleGeocoder.reverse_geocode @lat+','+@lon
+
+   @message= @info.full_address
+
    respond_to do |format|
       format.js
    end
